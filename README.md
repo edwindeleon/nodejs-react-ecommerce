26e8a562d15e4d23e8ba684c74a3aefa6af11726
Edwin De Leon <e.dleon@shopmundo.com>
2018-07-16 15:20:18 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nodejs-react-ecommerce
+Node.js and React.js Ecommerce using Cezerin as starting core. The goal is construct new features to the proyect.
---
+++ webpack.config.store.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['babel-polyfill', './src/store/client/index.js'],
+    theme: ['theme']
+  },
+  
+  performance: {
+    hints: false
+  },
+
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'theme'),
+    filename: 'assets/js/[name]-[chunkhash].js',
+    chunkFilename: 'assets/js/[name]-[chunkhash].js'
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'theme',
+          test: 'theme',
+          enforce: true
+        },
+      }
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }, {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+                modules: false,
+                importLoaders: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "assets/css/bundle-[contenthash].css",
+      chunkFilename: "assets/css/bundle-[contenthash].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: 'theme/index.html',
+      inject: 'body',
+      filename: 'assets/index.html'
+    }),
+    new webpack.BannerPlugin({
+      banner: `Created: ${new Date().toUTCString()}`,
+      raw: false,
+      entryOnly: false
+    })
+  ]
+};
